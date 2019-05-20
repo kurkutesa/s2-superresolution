a7bc40c3dfdbb3cec0f559175036a19691fe9a69
nekhtiari <n.ekhtiary@gmail.com>
2019-05-20 13:13:52 +0200

    switch from GDAL to rasterio

--- s2_tiles_supres.py
+++ s2_tiles_supres.py
@@ -1,12 +1,17 @@
 from __future__ import division
+
 import argparse
-import numpy as np
 import os
 import re
 import sys
-from osgeo import gdal, osr
 from collections import defaultdict
+
+import numpy as np
+import rasterio
+from rasterio.windows import Window
 from supres import DSen2_20, DSen2_60
+
+# from osgeo import gdal, osr
 
 # This code is adapted from this repository http://nicolas.brodu.net/code/superres and is distributed under the same
 # license.
@@ -14,9 +19,9 @@ from supres import DSen2_20, DSen2_60
 parser = argparse.ArgumentParser(description="Perform super-resolution on Sentinel-2 with DSen2. Code based on superres"
                                              " by Nicolas Brodu.",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("data_file",
-                    help="An input sentinel-2 data file. This can be either the original ZIP file, or the S2A[...].xml "
-                         "file in a SAFE directory extracted from that ZIP.")
+#parser.add_argument("data_file",
+#                    help="An input sentinel-2 data file. This can be either the original ZIP file, or the S2A[...].xml "
+#                         "file in a SAFE directory extracted from that ZIP.")
 parser.add_argument("output_file", nargs="?",
                     help="A target data file. See also the --save_prefix option, and the --output_file_format option "
                          "(default is GTiff).")
@@ -57,26 +62,25 @@ parser.add_argument("--save_prefix", default="",
                          "directory. The default of no prefix will save into the current directory. "
                          "Example: --save_prefix result/")
 
-
 args = parser.parse_args()
 globals().update(args.__dict__)
 
-if list_output_file_formats:
-    dcount = gdal.GetDriverCount()
-    for didx in range(dcount):
-        driver = gdal.GetDriver(didx)
-        if driver:
-            metadata = driver.GetMetadata()
-        if (gdal.DCAP_CREATE in (driver and metadata) and metadata[gdal.DCAP_CREATE] == 'YES' and
-        gdal.DCAP_RASTER in metadata and metadata[gdal.DCAP_RASTER] == 'YES'):
-            name = driver.GetDescription()
-            if "DMD_LONGNAME" in metadata:
-                name += ": " + metadata["DMD_LONGNAME"]
-            else:
-                name = driver.GetDescription()
-            if "DMD_EXTENSIONS" in metadata: name += " (" + metadata["DMD_EXTENSIONS"] + ")"
-            print(name)
-    sys.exit(0)
+# if list_output_file_formats:
+#    dcount = gdal.GetDriverCount()
+#    for didx in range(dcount):
+#        driver = gdal.GetDriver(didx)
+#        if driver:
+#            metadata = driver.GetMetadata()
+#        if (gdal.DCAP_CREATE in (driver and metadata) and metadata[gdal.DCAP_CREATE] == 'YES' and
+#        gdal.DCAP_RASTER in metadata and metadata[gdal.DCAP_RASTER] == 'YES'):
+#            name = driver.GetDescription()
+#            if "DMD_LONGNAME" in metadata:
+#                name += ": " + metadata["DMD_LONGNAME"]
+#            else:
+#                name = driver.GetDescription()
+#            if "DMD_EXTENSIONS" in metadata: name += " (" + metadata["DMD_EXTENSIONS"] + ")"
+#            print(name)
+#    sys.exit(0)
 
 if run_60:
     select_bands = 'B1,B2,B3,B4,B5,B6,B7,B8,B8A,B9,B11,B12'
@@ -91,26 +95,10 @@ if roi_lon_lat:
 else:
     roi_lon1, roi_lat1, roi_lon2, roi_lat2 = -180, -90, 180, 90
 
-if roi_x_y:
-    roi_x1, roi_y1, roi_x2, roi_y2 = [float(x) for x in re.split(',', roi_x_y)]
 
-raster = gdal.Open(data_file)
-
-ds10 = gdal.Open('/Users/nikoo.ekhtiari/Downloads/output/68df531c-2e83-4edf-91e3-3588f991d489/S2B_MSIL1C_20190508T071629_N0207_R006_T39SWV_20190508T110242.SAFE/GRANULE/L1C_T39SWV_A011329_20190508T071918/IMG_DATA/stack.tif')
-datasets = raster.GetSubDatasets();
-#tenMsets = []
-twentyMsets = []
-sixtyMsets = []
-unknownMsets = []
-for (dsname, dsdesc) in datasets:
-    #if '10m resolution' in dsdesc:
-        #tenMsets += [(dsname, dsdesc)]
-    if '20m resolution' in dsdesc:
-        twentyMsets += [(dsname, dsdesc)]
-    elif '60m resolution' in dsdesc:
-        sixtyMsets += [(dsname, dsdesc)]
-    else:
-        unknownMsets += [(dsname, dsdesc)]
+ds10 = rasterio.open('10m.tiff')
+ds20 = rasterio.open('20m.tif')
+ds60 = rasterio.open('60m.tif')
 
 # case where we have several UTM in the data set
 # => select the one with maximal coverage of the study zone
@@ -119,26 +107,28 @@ utm = select_UTM
 all_utms = defaultdict(int)
 xmin, ymin, xmax, ymax = 0, 0, 0, 0
 largest_area = -1
-# process even if there is only one 10m set, in order to get roi -> pixels
-# for (tmidx, (dsname, dsdesc)) in enumerate(tenMsets + unknownMsets):
-#     ds = gdal.Open(dsname)
-#     if roi_x_y:
-#         tmxmin = max(min(roi_x1, roi_x2, ds.RasterXSize - 1), 0)
-#         tmxmax = min(max(roi_x1, roi_x2, 0), ds.RasterXSize - 1)
-#         tmymin = max(min(roi_y1, roi_y2, ds.RasterYSize - 1), 0)
-#         tmymax = min(max(roi_y1, roi_y2, 0), ds.RasterYSize - 1)
-#         # enlarge to the nearest 60 pixel boundary for the super-resolution
-#         tmxmin = int(tmxmin / 6) * 6
-#         tmxmax = int((tmxmax + 1) / 6) * 6 - 1
-#         tmymin = int(tmymin / 6) * 6
-#         tmymax = int((tmymax + 1) / 6) * 6 - 1
-#         print("tmxmin=%d, tmxmax=%d,tmymin=%d,tmymax=%d:" % (tmxmin, tmxmax, tmymin,tmymax))
-#     elif not roi_lon_lat:
-#         tmxmin = 0
-#         tmxmax = ds.RasterXSize - 1
-#         tmymin = 0
-#         tmymax = ds.RasterYSize - 1
-#         print("Here I am1")
+
+
+def get_max_min(x1, y1, x2, y2):
+    tmxmin = max(min(x1, x2, ds10.width - 1), 0)
+    tmxmax = min(max(x1, x2, 0), ds10.width - 1)
+    tmymin = max(min(y1, y2, ds10.height - 1), 0)
+    tmymax = min(max(y1, y2, 0), ds10.height - 1)
+    # enlarge to the nearest 60 pixel boundary for the super-resolution
+    tmxmin = int(tmxmin / 6) * 6
+    tmxmax = int((tmxmax + 1) / 6) * 6 - 1
+    tmymin = int(tmymin / 6) * 6
+    tmymax = int((tmymax + 1) / 6) * 6 - 1
+    area = (tmxmax - tmxmin + 1) * (tmymax - tmymin + 1)
+    return tmxmin,tmymin,tmxmax,tmymax, area
+
+
+if roi_x_y:
+    roi_x1, roi_y1, roi_x2, roi_y2 = [float(x) for x in re.split(',', roi_x_y)]
+    xmin, ymin, xmax, ymax, area = get_max_min(roi_x1,roi_y1, roi_x2, roi_y2)
+else:
+    xmin, ymin, xmax, ymax = (0, 0, ds10.width, ds10.height)
+
 #     else:
 #         xoff, a, b, yoff, d, e = ds.GetGeoTransform()
 #         srs = osr.SpatialReference()
@@ -161,15 +151,7 @@ largest_area = -1
 #
 #         x1, y1 = to_xy(roi_lon1, roi_lat1)
 #         x2, y2 = to_xy(roi_lon2, roi_lat2)
-#         tmxmin = max(min(x1, x2, ds.RasterXSize - 1), 0)
-#         tmxmax = min(max(x1, x2, 0), ds.RasterXSize - 1)
-#         tmymin = max(min(y1, y2, ds.RasterYSize - 1), 0)
-#         tmymax = min(max(y1, y2, 0), ds.RasterYSize - 1)
-#         # enlarge to the nearest 60 pixel boundary for the super-resolution
-#         tmxmin = int(tmxmin / 6) * 6
-#         tmxmax = int((tmxmax + 1) / 6) * 6 - 1
-#         tmymin = int(tmymin / 6) * 6
-#         tmymax = int((tmymax + 1) / 6) * 6 - 1
+
 #     area = (tmxmax - tmxmin + 1) * (tmymax - tmymin + 1)
 #     print(area)
 #     current_utm = dsdesc[dsdesc.find("UTM"):]
@@ -186,42 +168,20 @@ largest_area = -1
 #         utm_idx = tmidx
 #         utm = dsdesc[dsdesc.find("UTM"):]
 # print(area)
+
 utm = 'UTM 39N'
 if list_UTM:
     print("List of UTM zones (with ROI coverage in pixels):")
     for u in all_utms:
         print("%s (%d)" % (u, all_utms[u]))
     sys.exit(0)
-xmin, ymin, xmax, ymax = 0,0,1000,1000
 print("Selected UTM Zone:", utm)
 print("Selected pixel region: xmin=%d, ymin=%d, xmax=%d, ymax=%d:" % (xmin, ymin, xmax, ymax))
-#print("Image size: width=%d x height=%d" % (xmax - xmin + 1, ymax - ymin + 1))
+print("Image size: width=%d x height=%d" % (xmax - xmin + 1, ymax - ymin + 1))
 
 if xmax < xmin or ymax < ymin:
     print("Invalid region of interest / UTM Zone combination")
     sys.exit(0)
-
-#selected_10m_data_set = None
-#if not tenMsets:
-#    selected_10m_data_set = unknownMsets[0]
-#else:
-#    selected_10m_data_set = tenMsets[utm_idx]
-selected_20m_data_set = None
-for (dsname, dsdesc) in enumerate(twentyMsets):
-    if utm in dsdesc:
-        selected_20m_data_set = (dsname, dsdesc)
-# if not found, assume the listing is in the same order
-# => OK if only one set
-if not selected_20m_data_set: selected_20m_data_set = twentyMsets[utm_idx]
-selected_60m_data_set = None
-for (dsname, dsdesc) in enumerate(sixtyMsets):
-    if utm in dsdesc:
-        selected_60m_data_set = (dsname, dsdesc)
-if not selected_60m_data_set: selected_60m_data_set = sixtyMsets[utm_idx]
-
-#ds10 = gdal.Open(selected_10m_data_set[0])
-ds20 = gdal.Open(selected_20m_data_set[0])
-ds60 = gdal.Open(selected_60m_data_set[0])
 
 
 def validate_description(description):
@@ -237,14 +197,14 @@ def validate_description(description):
 
 if list_bands:
     print("\n10m bands:")
-    for b in range(0, ds10.RasterCount):
-        print("- " + validate_description(ds10.GetRasterBand(b + 1).GetDescription()))
+    for b in range(0, ds10.count):
+        print("- " + validate_description(ds10.descriptions[b]))
     print("\n20m bands:")
-    for b in range(0, ds20.RasterCount):
-        print("- " + validate_description(ds20.GetRasterBand(b + 1).GetDescription()))
+    for b in range(0, ds20.count):
+        print("- " + validate_description(ds10.descriptions[b]))
     print("\n60m bands:")
-    for b in range(0, ds60.RasterCount):
-        print("- " + validate_description(ds60.GetRasterBand(b + 1).GetDescription()))
+    for b in range(0, ds60.count):
+        print("- " + validate_description(ds10.descriptions[b]))
     print("")
 
 
@@ -256,87 +216,71 @@ def get_band_short_name(description):
     return description[:3]
 
 
-validated_10m_bands = []
-validated_10m_indices = []
-validated_20m_bands = []
-validated_20m_indices = []
-validated_60m_bands = []
-validated_60m_indices = []
-validated_descriptions = defaultdict(str)
+def validate(data):
+    validated_bands = []
+    validated_indices = []
+    validated_descriptions = defaultdict(str)
+    for b in range(0, data.count):
+        desc = validate_description(data.descriptions[b])
+        name = get_band_short_name(desc)
+        if name in select_bands:
+            sys.stdout.write(" " + name)
+            select_bands.remove(name)
+            validated_bands += [name]
+            validated_indices += [b]
+            validated_descriptions[name] = desc
+    return validated_bands, validated_indices, validated_descriptions
 
-#sys.stdout.write("Selected 10m bands:")
-#for b in range(0, ds10.RasterCount):
-#    desc = validate_description(ds10.GetRasterBand(b + 1).GetDescription())
-#    shortname = get_band_short_name(desc)
-#    if shortname in select_bands:
-#        sys.stdout.write(" " + shortname)
-#        select_bands.remove(shortname)
-#        validated_10m_bands += [shortname]
-#        validated_10m_indices += [b]
-#        validated_descriptions[shortname] = desc
 
-validated_10m_indices = [0,1,2,3]
-validated_10m_bands = ['B2', 'B3', 'B4', 'B8']
-sys.stdout.write("\nSelected 20m bands:")
-for b in range(0, ds20.RasterCount):
-    desc = validate_description(ds20.GetRasterBand(b + 1).GetDescription())
-    shortname = get_band_short_name(desc)
-    if shortname in select_bands:
-        sys.stdout.write(" " + shortname)
-        select_bands.remove(shortname)
-        validated_20m_bands += [shortname]
-        validated_20m_indices += [b]
-        validated_descriptions[shortname] = desc
-sys.stdout.write("\nSelected 60m bands:")
-for b in range(0, ds60.RasterCount):
-    desc = validate_description(ds60.GetRasterBand(b + 1).GetDescription())
-    shortname = get_band_short_name(desc)
-    if shortname in select_bands:
-        sys.stdout.write(" " + shortname)
-        select_bands.remove(shortname)
-        validated_60m_bands += [shortname]
-        validated_60m_indices += [b]
-        validated_descriptions[shortname] = desc
+sys.stdout.write("Selected 10m bands:")
+validated_10m_bands, validated_10m_indices, dic_10m = validate(ds10)
+
+sys.stdout.write("Selected 20m bands:")
+validated_20m_bands, validated_20m_indices, dic_20m = validate(ds20)
+
+sys.stdout.write("Selected 60m bands:")
+validated_60m_bands, validated_60m_indices, dic_60m = validate(ds60)
+
+validated_descriptions_all = {**dic_10m, **dic_20m, **dic_60m}
+# validated_10m_indices = [0, 1, 2, 3]
+# validated_10m_bands = ['B2', 'B3', 'B4', 'B8']
+# validated_20m_indices = [0, 1, 2, 3, 4, 5]
+# validated_20m_bands = ['B5', 'B6', 'B7', 'B8A', 'B11', 'B12']
+# validated_60m_indices = [0, 1, 2]
+# validated_60m_bands = ['B1', 'B9', 'B10']
+
 sys.stdout.write("\n")
 
 if list_bands:
     sys.exit(0)
 
 # All query options are processed, we now require an output file
-if not output_file:
-    print("Error: you must provide the name of an output file. I will set it identical to the input...")
-    output_file = os.path.split(data_file)[1] + '.tif'
+#if not output_file:
+#    print("Error: you must provide the name of an output file. I will set it identical to the input...")
+#    output_file = os.path.split(data_file)[1] + '.tif'
     # sys.exit(1)
-
 
 output_file = save_prefix + output_file
 # Some HDR restrictions... ENVI file name should be the .bin, not the .hdr
 if output_file_format == 'ENVI' and (output_file[-4:] == '.hdr' or output_file[-4:] == '.HDR'):
     output_file = output_file[:-4] + '.bin'
 
+def data_final(data, term, x_mi, y_mi, x_ma, y_ma, n):
+    if term:
+        print(term)
+        d_final = np.rollaxis(
+            data.read(window=Window(col_off=x_mi, row_off=y_mi, width=x_ma - x_mi + n, height=y_ma - y_mi + n)), 0, 3)[
+                 :, :, term]
+    return d_final
 
-if validated_10m_indices:
-    #print("Loading selected data from: %s" % selected_10m_data_set[1])
-    print("xmin=%d, ymin=%d, xmax=%d, ymax=%d:" % (xmin, ymin, xmax, ymax))
-    print(validated_10m_indices)
-    data10 = np.rollaxis(
-        ds10.ReadAsArray(xoff=xmin, yoff=ymin, xsize=xmax - xmin + 1, ysize=ymax - ymin + 1, buf_xsize=xmax - xmin + 1,
-                         buf_ysize=ymax - ymin + 1), 0, 3)[:, :, validated_10m_indices]
-print("Done")
-if validated_20m_indices:
-    print("Loading selected data from: %s" % selected_20m_data_set[1])
-    data20 = np.rollaxis(
-        ds20.ReadAsArray(xoff=xmin // 2, yoff=ymin // 2, xsize=(xmax - xmin + 1) // 2, ysize=(ymax - ymin + 1) // 2,
-                         buf_xsize=(xmax - xmin + 1) // 2, buf_ysize=(ymax - ymin + 1) // 2), 0, 3)[:, :,
-             validated_20m_indices]
 
-if validated_60m_indices:
-    print("Loading selected data from: %s" % selected_60m_data_set[1])
-    data60 = np.rollaxis(
-        ds60.ReadAsArray(xoff=xmin // 6, yoff=ymin // 6, xsize=(xmax - xmin + 1) // 6, ysize=(ymax - ymin + 1) // 6,
-                         buf_xsize=(xmax - xmin + 1) // 6, buf_ysize=(ymax - ymin + 1) // 6), 0, 3)[:, :,
-             validated_60m_indices]
-
+if run_60:
+    data10 = data_final(ds10, validated_10m_indices, xmin, ymin, xmax, ymax, 1)
+    data20 = data_final(ds20, validated_20m_indices, xmin // 2, ymin // 2, xmax // 2, ymax // 2, 1 // 2)
+    data60 = data_final(ds60, validated_60m_indices, xmin // 6, ymin // 6, xmax // 6, ymax // 6, 1 // 6)
+else:
+    data10 = data_final(ds10, validated_10m_indices, xmin, ymin, xmax, ymax, 1)
+    data20 = data_final(ds20, validated_20m_indices, xmin // 2, ymin // 2, xmax // 2, ymax // 2, 1 // 2)
 
 if validated_60m_bands and validated_20m_bands and validated_10m_bands:
     print("Super-resolving the 60m data into 10m bands")
@@ -356,37 +300,37 @@ if sr20 is None:
     print("No super-resolution performed, exiting")
     sys.exit(0)
 
-if output_file_format != "npz":
-    revert_to_npz = True
-    driver = gdal.GetDriverByName(output_file_format)
-    if driver:
-        metadata = driver.GetMetadata()
-        if gdal.DCAP_CREATE in metadata and metadata[gdal.DCAP_CREATE] == 'YES':
-            revert_to_npz = False
-    if revert_to_npz:
-        print("Gdal doesn't support creating %s files" % output_file_format)
-        print("Writing to npz as a fallback")
-        output_file_format = "npz"
-    bands = None
-else:
-    bands = dict()
-    result_dataset = None
+# if output_file_format != "npz":
+#    revert_to_npz = True
+#    driver = gdal.GetDriverByName(output_file_format)
+#    if driver:
+#        metadata = driver.GetMetadata()
+#        if gdal.DCAP_CREATE in metadata and metadata[gdal.DCAP_CREATE] == 'YES':
+#            revert_to_npz = False
+#    if revert_to_npz:
+#        print("Gdal doesn't support creating %s files" % output_file_format)
+#        print("Writing to npz as a fallback")
+#        output_file_format = "npz"
+#    bands = None
+#else:
+#    bands = dict()
+#    result_dataset = None
 
-bidx = 0
-all_descriptions = []
-source_band = dict()
+#bidx = 0
+#all_descriptions = []
+#source_band = dict()
 
 
-def write_band_data(data, description, shortname=None):
-    global all_descriptions
-    global bidx
-    all_descriptions += [description]
-    if output_file_format == "npz":
-        bands[description] = data
-    else:
-        bidx += 1
-        result_dataset.GetRasterBand(bidx).SetDescription(description)
-        result_dataset.GetRasterBand(bidx).WriteArray(data)
+#def write_band_data(data, description, name=None):
+#    global all_descriptions
+#    global bidx
+#    all_descriptions += [description]
+#    if output_file_format == "npz":
+#        bands[description] = data
+#    else:
+#        bidx += 1
+#        result_dataset.GetRasterBand(bidx).SetDescription(description)
+#        result_dataset.GetRasterBand(bidx).WriteArray(data)
 
 
 if sr60 is not None:
@@ -401,30 +345,38 @@ if copy_original_bands:
 else:
     out_dims = sr.shape[2]
 
-
 sys.stdout.write("Writing")
-result_dataset = driver.Create(output_file, data10.shape[1], data10.shape[0], out_dims, gdal.GDT_Float64)
+# result_dataset = driver.Create(output_file, data10.shape[1], data10.shape[0], out_dims, gdal.GDT_Float64)
+
+print(" the super-resolved bands in %s" % output_file)
+profile = ds10.profile
+with rasterio.open('/tmp/example.tif', 'w' ,**profile) as ds10w:
+    for bi, bn in enumerate(validated_sr_bands):
+        profile.update(dtype=rasterio.float32)
+        ds10w.write(sr20[:,:,bi], indexes=bi+1)
+
 
 # Translate the image upper left corner. We multiply x10 to transform from pixel position in the 10m_band to meters.
-geot = list(ds10.GetGeoTransform())
-geot[0] += xmin * 10
-geot[3] -= ymin * 10
-result_dataset.SetGeoTransform(tuple(geot))
-result_dataset.SetProjection(ds10.GetProjection())
+# geot = list(ds10.GetGeoTransform())
+# geot[0] += xmin * 10
+# geot[3] -= ymin * 10
+# result_dataset.SetGeoTransform(tuple(geot))
+# result_dataset.SetProjection(ds10.GetProjection())
 
-if copy_original_bands:
-    sys.stdout.write(" the original 10m bands and")
+# result_dataset.(ds10.crs.wkt)
+
+# if copy_original_bands:
+#    sys.stdout.write(" the original 10m bands and")
     # Write the original 10m bands
-    for bi, bn in enumerate(validated_10m_bands):
-        write_band_data(data10[:, :, bi], validated_descriptions[bn])
+#    for bi, bn in enumerate(validated_10m_bands):
+#        write_band_data(data10[:, :, bi], validated_descriptions_all[bn])
+
 print(" the super-resolved bands in %s" % output_file)
-for bi, bn in enumerate(validated_sr_bands):
-    write_band_data(sr[:, :, bi], "SR" + validated_descriptions[bn], "SR" + bn)
+# for bi, bn in enumerate(validated_sr_bands):
+#    write_band_data(sr[:, :, bi], "SR" + validated_descriptions_all[bn], "SR" + bn)
 
-
-for desc in all_descriptions:
-    print(desc)
+# for desc in all_descriptions:
+#    print(desc)
 
 if output_file_format == "npz":
     np.savez(output_file, bands=bands)
-
--- test_gdal_vs_rasterio.py
+++ test_gdal_vs_rasterio.py
@@ -2,6 +2,38 @@ from osgeo import gdal, osr
 import rasterio
 import unittest
 
+from collections import defaultdict
+
+
+def validate_description(description):
+    m = re.match("(.*?), central wavelength (\d+) nm", description)
+    if m:
+        return m.group(1) + " (" + m.group(2) + " nm)"
+    return description
+
+def get_band_short_name(description):
+    if ',' in description:
+        return description[:description.find(',')]
+    if ' ' in description:
+        return description[:description.find(' ')]
+    return description[:3]
+
+
+def validate(data):
+    validated_bands = []
+    validated_indices = []
+    validated_descriptions = defaultdict(str)
+    for b in range(0, data.count):
+        desc = validate_description(data.descriptions[b])
+        name = get_band_short_name(desc)
+        if name in select_bands:
+            sys.stdout.write(" " + name)
+            select_bands.remove(name)
+            validated_bands += [name]
+            validated_indices += [b]
+            validated_descriptions[name] = desc
+    return validated_bands, validated_indices, validated_descriptions
+
 
 class TestStringMethods(unittest.TestCase):
 	def setUp(self):
@@ -33,6 +65,14 @@ class TestStringMethods(unittest.TestCase):
 		data10 = self.ds10.ReadAsArray()
 		data10r = self.ds10r.read()
 		self.assertEqual(data10.all(), data10r.all())
+
+	def test_desc(self):
+		validated_10m_indices_exm = [0, 1, 2, 3]
+		validated_10m_bands_exm = ['B2', 'B3', 'B4', 'B8']
+
+		validated_10m_bands, validated_10m_indices, dic_10m = validate(self.ds10r)
+		self.assertEqual(validated_10m_bands, validated_10m_bands_exm)
+		self.assertEqual(validated_10m_indices, validated_10m_indices_exm)
 
 
 if __name__ == '__main__':
