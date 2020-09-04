373bda25269c2e1d0d580fa61caa296e151592f5
Markus M <markus.mueller@up42.com>
2020-09-04 10:24:08 +0200

    Add timing info to tests (#41)

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # Hrm, if we don't delete the cache the linter skips all if its test cases as it has problems with our conftest
 rm -r .pytest_cache
 black .
-python -m pytest --pylint --pylint-rcfile=../../pylintrc --mypy --mypy-ignore-missing-imports --cov=src/
+python -m pytest --pylint --pylint-rcfile=../../pylintrc --mypy --mypy-ignore-missing-imports --cov=src/ --durations=0
 RET_VALUE=$?
 coverage-badge -f -o coverage.svg
 exit $RET_VALUE
